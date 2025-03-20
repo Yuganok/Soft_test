88fe478a23abaf7454c72a5ec807a349f3d645cf
Yuganok <60850867+Yuganok@users.noreply.github.com>
2025-03-20 17:57:33 +0200

    Initial commit

---
+++ src/test/java/triangle/TriangleTest.java
@@ -0,0 +1,36 @@
+package triangle;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleTest {
+    @Test
+    void testEquilateral() {
+        assertEquals("Equilateral", Triangle.classifyTriangle(100, 100, 100));
+    }
+
+    @Test
+    void testIsosceles() {
+        assertEquals("Isosceles", Triangle.classifyTriangle(100, 100, 50));
+        assertEquals("Isosceles", Triangle.classifyTriangle(50, 100, 100));
+        assertEquals("Isosceles", Triangle.classifyTriangle(100, 50, 100));
+    }
+
+    @Test
+    void testScalene() {
+        assertEquals("Scalene", Triangle.classifyTriangle(50, 60, 70));
+    }
+
+    @Test
+    void testNotATriangle() {
+        assertEquals("NotATriangle", Triangle.classifyTriangle(100, 50, 30));
+        assertEquals("NotATriangle", Triangle.classifyTriangle(1, 2, 3));
+    }
+
+    @Test
+    void testOutOfRange() {
+        assertEquals("Value of a is not in the range of permitted values.", Triangle.classifyTriangle(201, 100, 100));
+        assertEquals("Value of b is not in the range of permitted values.", Triangle.classifyTriangle(100, 0, 100));
+        assertEquals("Value of c is not in the range of permitted values.", Triangle.classifyTriangle(100, 100, 202));
+    }
+}
